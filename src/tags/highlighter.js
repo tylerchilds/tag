5668a403e0f4bb545ea3f90bcd63e7ed84b2ee4f
Tyler Childs <email@tychi.me>
2021-08-20 09:14:23 -0700

    refactor: de-dupe css that was duplicated

--- src/tags/highlighter.js
+++ src/tags/highlighter.js
@@ -6,13 +6,6 @@ highlighter.html((target) => {
 
   if(!target.css) {
     target.css = `
-      & {
-        display: inline-block;
-        position: relative;
-        z-index: 1;
-        white-space: nowrap;
-      }
-
       &[color="${color}"]::before {
         background: ${color};
         border-radius: ${n(4, 0, 100, 'px')};
@@ -22,11 +15,6 @@ highlighter.html((target) => {
         opacity: ${n(1, .2, .35)};
         transform: rotate(${n(1, -7, 7, 'deg')}) scale(${n(1, .95, 1.1)});
       }
-
-      & span {
-        position: relative;
-        z-index: 1;
-      }
     `
     highlighter.css(target.css)
     target.html = `<span>${target.innerHTML}</span>`
@@ -34,6 +22,20 @@ highlighter.html((target) => {
 
   return target.html
 })
+
+highlighter.css(`
+  & {
+    display: inline-block;
+    position: relative;
+    z-index: 1;
+    white-space: nowrap;
+  }
+
+  & span {
+    position: relative;
+    z-index: 1;
+  }
+`)
 
 function n(times = 1, min = 0, max = 1, suffix = '') {
   return [...new Array(times)]
