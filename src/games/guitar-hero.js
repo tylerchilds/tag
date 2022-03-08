f683c4ad7cc895139d7a13d1bb9f7ea0c78e06be
Tyler Childs <email@tychi.me>
2022-03-07 21:43:21 -0800

    feature: add theme export to guitar hero and catch in manuscript

--- src/games/guitar-hero.js
+++ src/games/guitar-hero.js
@@ -50,6 +50,44 @@ const chords = [
   "   xx",
 ]
 
+const colors = {
+  "x    ": 'green',
+  " x   ": 'red',
+  "  x  ": 'yellow',
+  "   x ": 'blue',
+  "    x": 'orange',
+
+  "xx   ": 'yellow',
+  "x x  ": 'orange',
+  "x  x ": 'chartruese',
+  "x   x": 'teal',
+
+  "xxx  ": 'amber',
+  "xx x ": 'indigo',
+  "xx  x": 'chartruese',
+  "xxxx ": 'magenta',
+
+  "xxx x": 'amber',
+  "xxxxx": 'green',
+  " xx  ": 'orange',
+  " x x ": 'violet',
+
+  " x  x": 'flame',
+  " xxx ": 'fuschia',
+  " xx x": 'orange',
+  " xxxx": 'chartruese',
+
+  "  xx ": 'green',
+  "  x x": 'amber',
+  "  xxx": 'amber',
+  "   xx": 'fuschia'
+}
+
+const shades = {
+  [1]: [6, 2],
+  [-1]: [1, 5],
+}
+
 $.ready(() => {
   $.write({ frames: {}, ready: false })
 })
@@ -75,27 +113,41 @@ function ready() {
   await ready()
   const { gamepads } = gamepad.read()
 
-  const activeChords = gamepads.map(x => toChords($, x))
-  const activeNotes = activeChords.map(x => getNote(x))
-  const activeOctaves = activeChords.map(x => getOctave(x))
+  const activeFrets = gamepads.map(x => toFrets($, x))
+  const activeChords = activeFrets.map(x => toChords($, x))
+  const activeNotes = activeChords.map(getNote)
+  const activeOctaves = activeChords.map(getOctave)
   const activeStrums = gamepads.map(x => toStrums($, x))
+
+  const activeColors = activeFrets.map(getColor)
+  const activeShades = activeStrums.map(getShades)
+  const activeThemes = activeColors.map((x, i) => toTheme($, {
+    color: x,
+    shades: activeShades[i]
+  }))
 
   $.write({
     time,
+    activeFrets,
     activeChords,
     activeNotes,
     activeOctaves,
-    activeStrums
+    activeStrums,
+    activeColors,
+    activeShades,
+    activeThemes
   })
 
   activeStrums.map((strum, i) => {
-    if(strum) {
+    if(isStrummed(strum)) {
+      const snapshot = () => playNote($, {
+        note: activeNotes[i],
+        octave: activeOctaves[i],
+        theme: activeThemes[i]
+      })
       throttle($, {
         key: `strum-${i}`,
-        activate: () => playNote($, {
-          note: activeNotes[i],
-          octave: activeOctaves[i]
-        }),
+        activate: snapshot,
         time,
         fps: 1000 / 4
       })
@@ -109,27 +161,39 @@ $.render(() => {
   const {
     activeChords,
     activeNotes,
-    activeStrums
+    activeStrums,
+    activeThemes
   } = $.read()
 
+  const classes = (i) => `note ${activeStrums[i] ? 'strummed' : ''}`
+  const styles = (i) => {
+    const { foreground, background } = activeThemes[i]
+    return `
+      background: ${background};
+      foreground: ${foreground};
+    `
+  }
+
   return activeChords.map((_chord, i) => `
-         <div class="note ${activeStrums[i] ? 'strummed' : ''}">
-          ${activeNotes[i]}
-         </div>
-        `).join('')
+    <div class="${classes(i)}" style="${styles(i)}">
+      ${activeNotes[i]}
+    </div>
+  `).join('')
 })
 
-function toChords(_$, flags) {
+function toFrets(_$, flags) {
   const pressed = value => value === 1 ? "x" : " "
-  const frets = flags.buttons.map(pressed)
-  const chord = fretMap.map(i => frets[i]).join('')
-  return chords.indexOf(chord) - 2
+  const frets = flags.buttons.map(pressed).slice(0, 5)
+  return fretMap.map(i => frets[i]).join('')
+}
+
+function toChords(_$, frets) {
+  return chords.indexOf(frets) - 2
 }
 
 function toStrums(_$, flags) {
-  const isStrummed = value => [-1, 1].includes(value)
   const [strumbar] = [...flags.axes].splice(-1)
-  return isStrummed(strumbar)
+  return strumbar
 }
 
 function getNote(chord) {
@@ -141,12 +205,33 @@ function getOctave(chord) {
   return rawOctave + baseOctave
 }
 
+function getColor(frets) {
+  return colors[frets] || 'silver'
+}
+
+function getShades(strum) {
+  return shades[strum] || [1, 7]
+}
+
+function toTheme(_$, flags) {
+  const { color, shades } = flags
+  const [x, y] = shades
+  return {
+    background: `var(--${color}${x})`,
+    foreground: `var(--${color}${y})`
+  }
+}
+
 function divide(x, n) {
   return Math.floor((x + n) / n) - 1;
 }
 
 function mod(x, n) {
   return ((x % n) + n) % n;
+}
+
+function isStrummed(value) {
+  return [-1, 1].includes(value)
 }
 
 function throttle($, flags) {
@@ -170,8 +255,14 @@ function throttle($, flags) {
 }
 
 function playNote(_$, flags) {
-  const { note, octave } = flags
+  const { note, octave, theme } = flags
   synth.triggerAttackRelease(`${note}${octave}`, "8n");
+
+  const html = document.querySelector('html')
+  html.style = `
+    --bg: ${theme.background};
+    --fg: ${theme.foreground};
+  `
 }
 
 $.style(`
