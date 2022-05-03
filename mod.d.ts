615be63b67cf8b93f0eb464fed9c326aceee81e2
Tyler Childs <email@tychi.me>
2022-05-02 21:40:47 -0700

    chore: typescript declarations, maybe

---
+++ mod.d.ts
@@ -0,0 +1,22 @@
+function Ready(callback: () => null) : null
+function Mount(target: HTMLElement) : null
+function Reader() : Record<string, unknown>
+function MergeHandler(state: Record<string, unknown>, payload: Record<string, unknown>) : null
+function Render(target: HTMLElement) : string | null
+function Styler(styles: string) : null
+function EventHandler(event: Record<string, unknown>) : null
+function EventDelegator(type: string, selector: string, EventHandler)
+function Writer(payload: Record<string, unknown>, MergeHandler) : null
+
+interface Tag {
+  ready: Ready;
+  selector: string;
+  mount: Mount;
+  read: Reader;
+  render: Render;
+  style: Styler;
+  on: EventDelegator;
+  write: Writer;
+}
+
+export function tag(selector: string, initialState: Record<string, unknown>): Tag
