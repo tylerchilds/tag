fca5488214ef481e28f8eb9f2937e798bd27fc8d
Tyler Childs <email@tychi.me>
2022-06-25 16:37:34 -0700

    feature: basic static file server

---
+++ server.js
@@ -0,0 +1,69 @@
+#!/usr/bin/env -S deno run -A
+import { serve } from "https://deno.land/std@0.114.0/http/server.ts";
+
+async function handleRequest(request) {
+  const { pathname } = new URL(request.url);
+
+  console.log("get", pathname);
+
+  if (pathname === '/') {
+    const file = await Deno.readFile('index.html')
+    return html(file)
+  }
+
+  let file = ''
+
+  try {
+    file = await Deno.readFile(`.${pathname}`)
+  } catch(e) {
+    try {
+      file = await Deno.readFile(`.${pathname}/index.html`)
+    } catch(e) {
+      return await blank(request)
+    }
+  }
+
+  const extension = pathname.split('.').slice(-1)
+
+  return new Response(file, {
+    headers: {
+      'content-type': getType(extension),
+    },
+  })
+}
+
+async function blank(request) {
+  const { pathname } = new URL(request.url);
+
+  try {
+    return html(`
+      <!doctype html>
+      <title>404: Not Found</title>
+      <p>Sorry, nothing to see here.
+    `
+    )
+  } catch(e) {
+    console.error(e)
+  }
+}
+
+function html(content) {
+  return new Response(content, {
+    headers: {
+      'content-type': getType('html'),
+    },
+  })
+}
+
+const types = {
+  'css': 'text/css; charset=utf-8',
+  'html': 'text/html; charset=utf-8',
+  'js': 'text/javascript; charset=utf-8'
+}
+
+function getType(ext) {
+  return types[ext] || types['html']
+}
+
+console.log("Listening on http://localhost:8000");
+serve(handleRequest);
