57265a3fccebecbf43bcbcd6a9aef3f542bf7221
Tyler Childs <email@tychi.me>
2022-09-15 18:23:10 -0700

    rip out past state logic in favor of the future

--- mod.js
+++ mod.js
@@ -1,25 +1,30 @@
-import ion from './src/vendor/ion/app.js'
 import { innerHTML } from 'https://esm.sh/diffhtml?bundle'
-import { deepEqual } from 'https://esm.sh/fast-equals@2.0.4?bundle'
+import bus from 'http://localhost:8042/client-library.js'
+import uuidv4 from './src/vendor/uuidv4.js';
 
-const cleanStates = {}
+const renderEvent = new Event('render');
+const createEvent = new Event('create');
 
-export function render(selector, renderer, dependencies = []) {
+const observableEvents = ['render']
+
+export const state = bus.state
+
+function render(selector, renderer) {
   let funk = () => null
 
-  ion.on('create', selector, (event) => {
-    funk = bus.reactive((event) => {
+  listen('create', selector, (event) => {
+    funk = bus.reactive(() => {
       const html = renderer(event.target)
       if(html) innerHTML(event.target, html)
     })
   })
 
-  ion.on('render', selector, (event) => {
+  listen('render', selector, (event) => {
     funk(event)
   })
 }
 
-export function style(selector, stylesheet) {
+function style(selector, stylesheet) {
   const styles = `
     <style type="text/css" data-tag=${selector}>
       ${stylesheet.replaceAll('&', selector)}
@@ -32,33 +37,16 @@ export function style(selector, stylesheet) {
 }
 
 export function read(selector) {
-  return ion.get(selector) || {}
+  return state[selector] || {}
 }
 
-export function write(selector, payload, middleware) {
-  ion.set(selector, payload, middleware)
+export function write(selector, payload, handler = (s, p) => ({...s,...p})) {
+  const current = state[selector]
+  state[selector] = handler(current || {}, payload);
 }
 
-export function on(selector1, eventName, selector2, callback) {
-  ion.on(eventName, `${selector1} ${selector2}`, callback)
-}
-
-function clean(id, selector, ...more) {
-  const selectors = [selector, ...more]
-  const cacheIndex = `${id}${new Error().stack}`
-
-  const cache = cleanStates[cacheIndex] || (function init(){
-    return cleanStates[cacheIndex] = {}
-  })()
-
-  return selectors.every(x => {
-    const previous = cache[x]
-    const current = ion.get(x)
-    const same = deepEqual(previous, current)
-
-    cleanStates[cacheIndex][x] = current
-    return same
-  })
+function on(selector1, eventName, selector2, callback) {
+  listen(eventName, `${selector1} ${selector2}`, callback)
 }
 
 export default function tag(selector, initialState = {}) {
@@ -74,3 +62,91 @@ export default function tag(selector, initialState = {}) {
   }
 }
 
+export function listen(type, selector, handler) {
+  const callback = (event) => {
+    if(event.target && event.target.matches && event.target.matches(selector)) {
+      handler.call(this, event);
+    }
+  };
+
+  document.addEventListener(type, callback, true);
+
+  if(observableEvents.includes(type)) {
+    observe(selector);
+  }
+
+  return function unlisten() {
+    if(type === 'render') {
+      disregard(selector);
+    }
+
+    document.removeEventListener(type, callback, true);
+  }
+}
+
+let selectors = []
+
+function observe(selector) {
+  selectors = [...new Set([...selectors, selector])];
+
+  maybeCreate([...document.querySelectorAll(selector)])
+  notify();
+}
+
+function disregard(selector) {
+  const index = selectors.indexOf(selector);
+  if(index >= 0) {
+    selectors = [
+      ...selectors.slice(0, index),
+      ...selectors.slice(index + 1)
+    ];
+  }
+}
+
+function notify() {
+  const subscribers = getSubscribers(document);
+  dispatchRender(subscribers);
+}
+
+function maybeCreate(targets) {
+  targets
+    .filter(x => !x.reactive)
+    .forEach(dispatchCreate)
+}
+
+function getSubscribers(node) {
+  if(selectors.length > 0)
+    return [...node.querySelectorAll(selectors.join(', '))];
+  else
+    return []
+}
+
+function dispatchRender(subscribers) {
+  subscribers.map(s => {
+    if(!s.id) s.id = uuidv4()
+    s.dispatchEvent(renderEvent)
+  });
+}
+
+function dispatchCreate(target) {
+  target.dispatchEvent(createEvent)
+  target.reactive = true
+}
+
+const config = { childList: true, subtree: true };
+
+function mutationObserverCallback(mutationsList, observer) {
+  const subscriberCollections = [...mutationsList]
+    .map(m =>	getSubscribers(m.target));
+
+  const targets = subscriberCollections
+    .flatMap(x =>x)
+
+  maybeCreate(targets)
+
+  subscriberCollections.forEach(dispatchRender);
+};
+
+const observer = new MutationObserver(mutationObserverCallback);
+
+observer.observe(document.body, config);
